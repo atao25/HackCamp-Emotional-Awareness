c4c6a8bc8d2fbc89b31f208ee24cb56c7782325b
Allison Tao <allisontao04@gmail.com>
2023-11-19 10:14:04 -0800

    added methods to gui

--- EmotionsGUI.java
+++ EmotionsGUI.java
@@ -8,28 +8,56 @@ import java.util.Arrays;
 
 public class EmotionsGUI {
 
-    private JFrame profile;
+    // login frame
+    private JFrame profileFrame;
+    private Dimension profileDimension;
+    private JPanel profilePanel;
 
 
 
-
-
+    // first layer emoji frame
     private JFrame primary;
+    private JButton happy;
+    private JButton sad;
+    private JButton disgust;
+    private JButton fear;
+    private JButton anger;
+    private JButton surprise;
 
 
 
-
-
+    // second layer emoji frame
     private JFrame secondary;
 
 
 
-    public EmotionsGUI() {
-
-    }
-
-
     public static void main (String[] args) {
         new EmotionsGUI();
+        System.out.println("hi");
     }
+
+    public EmotionsGUI() {
+        profileFrame = new JFrame();
+        profileDimension = new Dimension(600, 600);
+        profileFrame.setTitle("Emotional Awareness");
+        profileFrame.setSize(profileDimension.width, profileDimension.height);
+        profilePanel = setUpProfilePanel();
+
+
+        profileFrame.add(profilePanel);
+
+    }
+
+    public JPanel setUpProfilePanel() {
+        profilePanel = new JPanel();
+
+
+        return profilePanel;
+    }
+
+   
+
+
+
+
 } 
