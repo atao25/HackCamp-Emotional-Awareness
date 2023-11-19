160b73968d00cdb66cd7292ae58e654b93ab7de8
Allison Tao <allisontao04@gmail.com>
2023-11-19 09:43:33 -0800

    added gui

---
+++ EmotionsGUI.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+
+
+public class EmotionsGUI {
+
+    private JFrame profile;
+
+
+
+
+
+    private JFrame primary;
+
+
+
+
+
+    private JFrame secondary;
+
+
+
+    public EmotionsGUI() {
+
+    }
+
+
+    public static void main (String[] args) {
+        new EmotionsGUI();
+    }
+} 
