99fa7b5ed5fde89df5434c317081dd57f9519b62
Allison Tao <allisontao04@gmail.com>
2023-11-19 08:58:51 -0800

    added methods to PrimaryEmotion class

--- primaryEmotion.java
+++ primaryEmotion.java
@@ -1,5 +1,23 @@
-public class primaryEmotion {
+public class PrimaryEmotion {
 
-    private List<secondaryEmotion> secondaryEmotions;
+    private List<SecondaryEmotion> secondaryEmotions;
     private String name;
+
+    public PrimaryEmotion(String name, List<secondaryEmotion> secondaryEmotions) {
+        this.secondaryEmotions = new ArrayList()<>;
+        this.name = name;
+    }
+
+
+    public void addEmotion(secondaryEmotion emotion) {
+        secondaryEmotions.add(emotion);
+    }
+
+    public ArrayList<secondaryEmotion> getEmotionList() {
+        return secondaryEmotions;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
