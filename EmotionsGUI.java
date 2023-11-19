f93841a8ee04206502a0d39d3876ceb247c7945d
Allison Tao <allisontao04@gmail.com>
2023-11-19 10:50:08 -0800

    created methods

--- EmotionsGUI.java
+++ EmotionsGUI.java
@@ -12,6 +12,8 @@ public class EmotionsGUI {
     private JFrame profileFrame;
     private Dimension profileDimension;
     private JPanel profilePanel;
+    private JLabel title;
+    private JLabel message;
 
 
 
@@ -23,11 +25,13 @@ public class EmotionsGUI {
     private JButton fear;
     private JButton anger;
     private JButton surprise;
+    private JButton back1;
 
 
 
     // second layer emoji frame
     private JFrame secondary;
+    private JButton back2;
 
 
 
@@ -39,24 +43,42 @@ public class EmotionsGUI {
     public EmotionsGUI() {
         profileFrame = new JFrame();
         profileDimension = new Dimension(600, 600);
-       //profileFrame.setTitle("Emotional Awareness");
+        profileFrame.setTitle("Emotional Awareness App");
         profileFrame.setSize(profileDimension.width, profileDimension.height);
         profilePanel = setUpProfilePanel();
         profileFrame.setVisible(true);
-
-
         profileFrame.add(profilePanel);
+        makeProfileDetails();
+        profileFrame.pack();
+
+        profileFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       // profileFrame.setResizable(false);
 
     }
 
     public JPanel setUpProfilePanel() {
         profilePanel = new JPanel();
-        profilePanel.setLayout(new GridLayout(0, 1, 0, 12));
+        profilePanel.setLayout(new GridLayout(0, 1, 0, 1));
         profilePanel.setBorder(BorderFactory.createEmptyBorder(50, 120, 50, 120));
 
 
 
         return profilePanel;
+    }
+
+    public void makeProfileDetails() {
+        title = new JLabel("EMOTIONAL AWARENESS");
+        title.setFont(new Font(Font.SERIF, Font.BOLD, 25));
+        title.setVerticalAlignment(JLabel.TOP);
+        title.setHorizontalAlignment(JLabel.CENTER);
+        message = new JLabel("The ability to understand feelings ...");
+        message.setHorizontalAlignment(JLabel.CENTER);
+
+
+
+
+        profilePanel.add(title);
+        profilePanel.add(message);
     }
 
    
